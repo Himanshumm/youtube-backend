ca6814dc0a57b46367e3a61962d0be28e48bda3c
Himanshu Raj <himanshuraj.hr21@gmail.com>
2024-07-23 21:40:45 +0530

    connect to mongo DB

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,14 +1,4 @@
-// require('dotenv').config({ path:'./env'})   degrades code consistency
-
-import dotenv from "dotenv"
-import connectDB from "./db/index.js";
-
-dotenv.config({
-    path: './env'
-})
-
-connectDB()
-/* (
+(
   async () =>{
     try {
       await  mongoose.connect( `${process.env.MONGODB_URI}/${DB_NAME}`)
@@ -30,4 +20,4 @@ app.listen(process.env.PORT,()=>{
         
     }
   }
-)() */
+)()
